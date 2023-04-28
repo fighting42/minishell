3b328f674ffdeb0cae02bedaacec6a9784caa464
dapark <sharon1221@naver.com>
2023-04-28 17:05:26 +0900

    merge again

--- includes/parsing.h
+++ includes/parsing.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yejinkim <yejinkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: dapark <dapark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 15:17:53 by yejinkim          #+#    #+#             */
-/*   Updated: 2023/04/26 12:47:24 by yejinkim         ###   ########seoul.kr  */
+/*   Updated: 2023/04/26 18:23:52 by dapark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,21 @@ typedef struct s_dollar
 }	t_dollar;
 
 int			check_sep(char c, char *sep);
+int			chk_whole_quote(char *str);
+int			chk_quote_one(char *str, int flag);
+int			is_not_ok_sep(char *str, char *sep);
+int			count_colon(char *str);
+t_dollar	*chk_env(char *str, t_env *env);
+int			quote_status(char c, int quote);
+char		*trans_env(t_env *env, char *str, int start, int size);
+t_cmdline	*parsing(char *str, t_env *env);
+char		*remove_quote(char *str);
+int			check_sep(char c, char *sep);
 int			length_str(char *str, char *sep, int k, int flag);
 char		*make_str(char *str, char *sep, int k, int flag);
 int			count_str(char *str, char *sep);
 char		**parse_split(char *str, int count, t_dollar *env_var);
-char		*trans_env(char **envp, char *str, int start, int size);
-int			quote_status(char c, int quote);
-t_cmdline	*parsing(char *str, char **envp);
-t_dollar	*chk_env(char *str, char **envp);
+t_token		*create_token();
+void		append_token(t_token *head, t_token *curr, char *value, int *r_flag);
 
 #endif
